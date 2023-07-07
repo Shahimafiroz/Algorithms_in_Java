75b79fd8dd1c2604c40e44029e4180609ec250ef
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-07-07 23:19:35 +0530

    typecasting

--- TypeCasting/src/Main.java
+++ TypeCasting/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Scanner scan = new Scanner(System.in);
+//        int numm = 3.456f; this is wrong
+
+//        System.out.println("Enter a double ");
+//        double x = scan.nextDouble();
+//        int numm = (int)(x);
+        int numm = (int) (3.456f); // this is type casting now
+        System.out.println(numm);
+
     }
 }
