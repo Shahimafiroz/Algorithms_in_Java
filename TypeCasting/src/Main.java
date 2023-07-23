0de1a29b254d509e56ac567887bef0e91bbb61e3
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-07-23 16:03:47 +0530

    conditionals

--- TypeCasting/src/Main.java
+++ TypeCasting/src/Main.java
@@ -5,12 +5,35 @@ public class Main {
 
 //        Scanner scan = new Scanner(System.in);
 //        int numm = 3.456f; this is wrong
-
 //        System.out.println("Enter a double ");
 //        double x = scan.nextDouble();
 //        int numm = (int)(x);
-        int numm = (int) (3.456f); // this is type casting now
-        System.out.println(numm);
+//        int numm = (int) (3.456f); // this is type casting now
+//        System.out.println(numm);
+
+
+
+// Automatic type promotion
+//        int a = 257;
+//        byte b = (byte) a;
+//        System.out.println(b);
+//        byte a = 40 ;
+//        byte b = 50 ;
+//        byte c = 100;
+//        byte d = (byte) ((byte) a*b/c);
+//        int d =  a*b/c;
+//        System.out.println(d);
+
+
+// ASCII (American Standard Code for Information Interchange) is the most common character
+// encoding format for text data in computers and on the internet. In standard ASCII-encoded data,
+// there are unique values for 128 alphabetic, numeric or special additional characters and control codes.
+
+        int number = 'A';
+        System.out.println(number);
+
+
+
 
     }
 }
