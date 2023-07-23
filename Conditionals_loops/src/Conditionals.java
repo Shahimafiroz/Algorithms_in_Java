a7fa5a19ad0a918eef3d7d80dfaee495c3dd24ab
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-07-23 16:31:57 +0530

    Loops

---
+++ Conditionals_loops/src/Conditionals.java
@@ -0,0 +1,23 @@
+import  javax.swing.JOptionPane;
+
+
+public class Conditionals {
+    public static void main(String[] args) {
+       double salary = Integer.parseInt(JOptionPane.showInputDialog("Enter your salary : "));
+       double bonus;
+       if(salary >= 50000 ){
+           bonus = 0.1 * salary;
+           salary = salary + bonus;
+        JOptionPane.showMessageDialog(null , "Your salary for this month is " +salary+" and the bonus is " +bonus+ " . ");
+       }else if(salary >= 100000){
+           bonus = 0.2 *salary;
+           salary = salary+bonus;
+            JOptionPane.showMessageDialog(null , "Your salary for this month is " +salary+" and the bonus is " +bonus+ " .");
+        }else{
+           bonus = 0.3 *salary;
+           salary = salary+bonus;
+           JOptionPane.showMessageDialog(null , "Your salary for this month is " +salary+" and the bonus is " +bonus+ " .");
+        }
+
+    }
+}
