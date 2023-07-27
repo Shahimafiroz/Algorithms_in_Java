cd56382e74471aecdc995c2d8e453e9a159b9e5e
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-07-27 10:14:41 +0530

    for

--- Conditionals_loops/src/Loops_doWhile.java
+++ Conditionals_loops/src/Loops_doWhile.java
@@ -1,3 +1,4 @@
+import  java.util.Scanner;
 /*In do while loop the block of code is at least executed once and then the condition is checked
 syntax:-
 
@@ -8,8 +9,15 @@ syntax:-
 
  */
 
+import java.util.Scanner;
 public class Loops_doWhile {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        do {
+            System.out.print("Enter Your name : ");
+            name = scanner.nextLine();
+        }while (name.isBlank());
+        System.out.println("Hello "+ name );
     }
 }
--- Conditionals_loops/src/Loops_for.java
+++ Conditionals_loops/src/Loops_for.java
@@ -1,12 +1,22 @@
 // for loop = executes a block of code a limited amount of times
 
+import java.util.Scanner;
+
 public class Loops_for {
     public static void main(String[] args) {
-      int a = 5;
-       for( int i = 0;  i< a  ; i --){
-           Object[] options = {"Yes, please",
-                   "No, thanks",
-                   "No eggs, no ham!"};
-       }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the count : ");
+        int count = scan.nextInt();
+//        for (int i=0 ; count >= i; i++){
+//            System.out.println(i);
+//        }
+        for (int i = count ; i >= 0; i-- ){
+            System.out.println(i);
+        }
+//        for (int i = count ; i >= 0; i-=2 ){
+//            System.out.println(i);
+//        }
+         System.out.println("HAPPY NEW YEAR !!!");
+
     }
 }
