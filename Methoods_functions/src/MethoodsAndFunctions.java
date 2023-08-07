4b5b708dc5de8aeab86fcd2dde462c0d7dbee965
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-08-07 23:14:10 +0530

    practice

--- Methoods_functions/src/MethoodsAndFunctions.java
+++ Methoods_functions/src/MethoodsAndFunctions.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class MethoodsAndFunctions {
     public static void main(String[] args) {
         sum();
         int ot = sumdrum();
--- Methoods_functions/src/Swap.java
+++ Methoods_functions/src/Swap.java
@@ -1,5 +1,11 @@
-public class Mthods_funcs {
+public class Swap {
     public static void main(String[] args) {
+        int a= 20;
+        int b = 30;
+        int temp =a  ;
+        a = b ;
         
+
+
     }
 }
