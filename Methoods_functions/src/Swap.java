33a306ffd27f1bb7bfbb656789eb60c483899786
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-08-09 23:34:39 +0530

    practice

--- Methoods_functions/src/Swap.java
+++ Methoods_functions/src/Swap.java
@@ -2,10 +2,26 @@ public class Swap {
     public static void main(String[] args) {
         int a= 20;
         int b = 30;
+        System.out.println("a: "+ a+ " b: "+ b +" .");
+        //swap numbers code
+//        int temp =a  ;
+//        a = b ;
+//        b=temp;
+//        System.out.println("a: " + a + " b: " + b + " .");
+        swp(a , b );
+        System.out.println("a: " + a + " b: " + b + " .");
+
+    }
+
+    public static void swp (int a , int b){
+
         int temp =a  ;
         a = b ;
-        
+        b=temp;
 
 
     }
+
+
+
 }
