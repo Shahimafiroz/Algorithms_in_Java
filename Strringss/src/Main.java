12acd51981ec42c7d29c2af79fe9b3d57b8cef08
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-08-30 00:14:51 +0530

    strings

---
+++ Strringss/src/Main.java
@@ -0,0 +1,8 @@
+// string is a reference data type that can store one or more characters.
+// reference data types have access to some useful methods
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
