212e91c6188ae557e426f9215a105ab9e33b5439
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2024-03-04 23:31:52 +0530

    arrays

--- Arrays/src/ReverseAnArray/linerTCSol.java
+++ Arrays/src/ReverseAnArray/linerTCSol.java
@@ -14,13 +14,28 @@ public class linerTCSol {
             Input_array[i] = sc.nextInt();
         }// end of for
 
-        System.out.println(Reverse(Input_array));
+//        System.out.println(Reverse(Input_array)); ---> wrong
+
+        int[] reversedArray = Reverse(Input_array);
+        System.out.println("Reversed array:");
+        for (int num : reversedArray) {
+            System.out.print(num + " ");
+        }
     }
 
 
     public static int[] Reverse (int [] arr)
-    {
-        
+    {   int temp;
+        int j = arr.length-1;
+        for (int i = 0 ; i < arr.length/2 ; i++){
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j = j -1 ;
+            System.out.println("a[i] = : " +arr[i]+".");
+            System.out.println("a[j] = : " +arr[j]+".");
+        }
+
 
 
         return arr;
